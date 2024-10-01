654d50533c832166e887c042093af921ad2023f6
gkhulordava1 <goga.khulordavaDOIT@geolab.edu.ge>
2024-10-01 20:03:03 +0400

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,98 @@
+﻿
+// N1
+
+//for (int i = 0; i <= 100; i++)
+//{
+//    Console.WriteLine(i);
+//}
+
+
+// N2
+
+//for (int i = 0; i<=50; i++)
+
+//{
+//    if (i % 2 == 0)
+//    {
+//        if(i != 0)
+//        {
+//            Console.WriteLine(i);
+//        }
+//    }
+//}    
+
+// N3
+
+//int x = 0;
+//int i = 0;
+//while (i <= 50)
+//{
+//    x += i;
+//    i ++;
+//}
+//    Console.WriteLine(x);
+
+
+// N4
+
+//int i = 0;
+//int x = 0;
+
+//while (i < 5)
+//{
+//    Console.Write("Enter a Number: ");
+//    int y = int.Parse(Console.ReadLine());
+//    x += y;
+//    i++;
+//}
+//Console.WriteLine(x);
+
+
+
+// N5
+
+//int i;
+//do
+//{
+//    Console.Write("Enter a Number: ");
+//    i = int.Parse(Console.ReadLine());
+//}
+//while (i >= 10);
+
+
+// N6
+
+
+//int i = 0;
+//string answer;
+
+//do
+//{
+//    Console.WriteLine("Do you want to continue?");
+//    answer = Console.ReadLine(); 
+
+//    if (answer.ToLower() == "yes")
+//    {
+//        Console.WriteLine("Continuing...");
+//    }
+//    else if (answer.ToLower() == "no")
+//    {
+//        Console.WriteLine("Stopping...");
+//        break;
+//    }
+//    else
+//    {
+//        Console.WriteLine("The program has stopped.");
+//    }
+//    i++;
+//}
+//while (i < 3);
+
+
+// N7
+
+
+
+
+
+
